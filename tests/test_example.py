3edd9e7372b4ad570d2948b10be5453dd76ef7bf
Виталий <ipbvv@mail.ru>
2025-02-04 04:57:03 +0300

    Update test_example Add test_data

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,5 @@
 from hexlet_pytest.example import reverse
+from pathlib import Path
 
 
 def test_reverse():
@@ -7,3 +8,20 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent /'test_data'/filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_reverse_long():
+    text = read_file('data_1.txt')
+    print(text)
+    result = reverse(text)
+    print(result)
+    correct_result = read_file('result_1.txt')
+    assert result == correct_result
